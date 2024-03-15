78719c374faab319e26bb5cf0a6a23c2863bfd78
Jonathan Hsu <johnnhsu1031@gmail.com>
2024-03-15 00:01:19 -0700

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Jonathan Hsu on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
